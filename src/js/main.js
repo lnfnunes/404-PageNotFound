8c94923bd83e68a6e1ea7864585fb06425766aba
Luis Felipe Souza <souza@magnetis.com.br>
2016-10-24 21:02:21 -0200

    Add app files to gh-pages

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+
+var React = require('react'),
+    ReactDOM = require('react-dom'),
+    Feed = require('./components/Feed');
+
+ReactDOM.render(
+  <Feed />,
+  document.getElementById('app')
+);
