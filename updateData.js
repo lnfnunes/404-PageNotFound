87b20728c3d7e53eae5c016965fd58221ce1b1d1
Leandro Nunes <leandrof@ciandt.com>
2017-03-07 18:54:55 -0300

    Keep previous votes when updating DB

--- updateData.js
+++ updateData.js
@@ -1,65 +1,98 @@
 const admin = require('firebase-admin');
 const axios = require('axios');
+const _ = require('lodash');
 
 const serviceAccount = require('./serviceAccountKey.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://404-pagenotfound.firebaseio.com',
 });
-
 const db = admin.database();
 const ref = db.ref('feed');
-let arrLinks;
-ref
-  .orderByChild('title')
-  .once('value', function(snap) {
-    const links = snap.val();
-    try {
-      arrLinks = Object.keys(links).map(key => links[key]);
-    } catch (err) {
-      arrLinks = [];
-    }
-    console.log('initial data:', arrLinks.length);
 
-    fetchListLinks()
-      .then(parseLinks)
-      .then(syncAllLinks)
-      .catch(err => console.error(err))
-  }
-);
+const reParseMarkdownLinks = /\[(.*)\]\((.*)\)/gmi;
 
-const reMdLinks = /\[(.*)\]\((.*)\)/gmi;
-function fetchListLinks() {
+let arrFbLinks;
+getFirebaseLinks(arrFbLinks)
+  .then(data => {
+    console.log('==============================');
+    console.log('Firebase DB Links:', data.length);
+    console.log('==============================\n');
+
+    arrFbLinks = data;
+    return data;
+  })
+  .then(getGhLinks);
+
+function getFirebaseLinks() {
+  return new Promise((resolve, reject) => {
+    ref
+      .orderByChild('title')
+      .once('value', snap => {
+        // Store Firebase DB links
+        const fbLinks = snap.val();
+        const arr =
+          fbLinks
+            ? Object.keys(fbLinks).map(key => fbLinks[key])
+            : []
+        resolve(arr);
+      }
+    );
+  });
+}
+
+function getGhLinks() {
+  fetchGhLinks()
+    .then(parseLinks)
+    .then(data => {
+      console.log('==============================');
+      console.log('GH markdown Links:', data.length);
+      console.log('==============================\n');
+      return data;
+    })
+    .then(setLinkVotesAdapter)
+    .then(syncAllLinks)
+    .catch(err => console.error('getGhLinks ERROR:', err))
+}
+
+function fetchGhLinks() {
   return axios.get('https://raw.githubusercontent.com/lnfnunes/404-PageNotFound/master/LIST.md')
     .then(response => response.data)
     .catch(err => console.error('fetchListLinks ERROR:', err))
 }
-
 function parseLinks(str) {
   return Promise.resolve(
     str
-      .match(reMdLinks)
+      .match(reParseMarkdownLinks)
       .map(strLinkToJsonAdapter)
   )
 }
 function strLinkToJsonAdapter(link) {
   return JSON.parse(
-    link.replace(reMdLinks, JSON.stringify({
+    link.replace(reParseMarkdownLinks, JSON.stringify({
       title: '$1',
       url: '$2',
-      voteCount: 0
     }))
   );
 }
+function setLinkVotesAdapter(arr) {
+  return arr.map(link => {
+    const linkOnDB = _.find(arrFbLinks, {title: link.title});
+    const votes = linkOnDB ? linkOnDB.voteCount : 0;
 
-function syncAllLinks(arrListLinks) {
+    return Object.assign(link, {
+      voteCount: votes,
+    });
+  });
+}
+
+function syncAllLinks(arr) {
   // Reset DB
   ref.set({});
-
-  syncPromise(arrListLinks, addLink);
+  syncPromise(arr, addLink);
 }
 function addLink(link) {
-  console.log(`Adding ${link.title}...`);
+  console.log(`Adding ${link.title} [${link.voteCount}]...`);
 
   return new Promise((resolve, reject) => {
     ref.push().set(link, err =>
