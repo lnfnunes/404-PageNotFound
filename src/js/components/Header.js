39e18190f6990c6531b619c0c20c4a209429ea63
Leandro Nunes <leandrof@ciandt.com>
2017-03-11 01:06:14 -0300

    Fix logo img path

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -2,7 +2,7 @@ const React = require('react');
 
 const Header = () => (
   <div className="text-center">
-    <img src="dist/img/logo.png" alt="404 Page Not Found" />
+    <img src="./img/logo.png" alt="404 Page Not Found" />
   </div>
 );
 
